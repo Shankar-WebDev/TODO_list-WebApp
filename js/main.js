e78d0689b3c634ca2086de96f6931cb9a903771f
Sankaranarayanan <sankar241101@gmail.com>
2024-09-30 20:10:19 +0530

    Day 1 Code

---
+++ js/main.js
@@ -0,0 +1,9 @@
+import ToDolist from "./todolist";
+import ToDoItem from "./todoitem";
+
+
+
+const ToDoList = new ToDoList();
+
+
+//launch app
---
+++ js/todoitem.js
@@ -0,0 +1,20 @@
+export default class ToDoItem {
+  constructor() {
+    this._id = null;
+    this._item = null;
+  }
+
+  getId() {
+    return this._id;
+  }
+  setId() {
+    this._id = id;
+  }
+
+  getItem() {
+    return this._item;
+  }
+  setItem(item) {
+    this_item = item;
+  }
+}
